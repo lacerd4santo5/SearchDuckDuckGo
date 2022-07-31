41590cee7506a4cf56901d65d78f36aedb8ab127
lacerd4santo5 <lacerd4@gmail.com>
2022-07-31 12:35:07 -0300

    Find to Form

--- cypress/integration/SearchBitcoin.feature
+++ cypress/integration/SearchBitcoin.feature
@@ -2,20 +2,20 @@ Feature: SearchBitcoin
 
     Como usuário, desejo pesquisar Bitcoin no buscador DuckDuckGo
     Para que possa me informar
-
+   
 Scenario: Pesquisando e utilizando ENTER
     Given que eu acesso o site
     When escrevo "Bitcoin"
     And aperto "ENTER"
     Then devo ver mais de "10" resultados
-@focus
+
 Scenario: Pesquisando e utilizando clicando na lupa
     Given que eu acesso o site
     When escrevo "Bitcoin"
     And aperto "na lupa"
-    Then devo ver mais de "2" resultados
-
+    Then devo ver mais de "10" resultados
+@focus
 Scenario: Pesquisando pelo formulário
     Given que eu esceva o site
     And concacteno com "Bitcoin"
-    Then devo ver os "20" resultados
+    Then devo ver mais de "10" resultados
--- cypress/integration/SearchBitcoin/steps.js
+++ cypress/integration/SearchBitcoin/steps.js
@@ -2,17 +2,24 @@ Given(/^que eu acesso o site$/, () => {
 	cy.visit('/');
 });
 
+Given(/^que eu esceva o site$/, () => {
+	return true;
+});
+
+When(/^concacteno com "([^"]*)"$/, (concate) => {
+	cy.visit('/'+concate)
+});
 
 When(/^escrevo "([^"]*)"$/, (interesse) => {
-	
+	console.log(interesse);
 });
 
 When(/^aperto "([^"]*)"$/, (pesquisar) => {
 	if (pesquisar === 'ENTER') {
-		cy.get('#search_form_input_homepage').type('Bitcoin{enter}');
+		cy.get('#search_form_input_homepage').type(`${interes}{enter}`);
 	}
 	else {
-		cy.get('#search_form_input_homepage').type('Bitcoin')
+		cy.get('#search_form_input_homepage').type(interes);
 		cy.get('#search_button_homepage').click();
 	}
 	;
@@ -20,7 +27,7 @@ When(/^aperto "([^"]*)"$/, (pesquisar) => {
 
 Then(/^devo ver mais de "([^"]*)" resultados$/, (result) => {
 	cy.get('div.nrn-react-div').should(($p) => {
-		expect($p).to.be.above(1);		
+		expect($p).to.be.lengthOf(result);		
 	});
 });
 
